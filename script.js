1af4bc11f57da7516d1f7e3d3cdf019a02ceffc3
MCeciliaLuna <lunama.cecilia@live.com>
2023-05-26 20:43:07 -0300

    función que recoge un autor y filtra los libros de la misma lista

--- script.js
+++ script.js
@@ -21,7 +21,18 @@ console.table(books)
 
 //Primera parte: dado un array de libros, implementar una función filterBooksByAuthor( ) que reciba por parámetros dicho array y un string con el nombre de un autor, y devuelva un nuevo array con libros del autor indicado.
 
+console.log("%cSearch by Authors", "background-color: #ee9b00; color: white; font-size: 15px; text-align: center ; padding: 10px ;  margin: 5px")
 
+let searchAuthor = 'Gabriel García Márquez'
+
+const filterBooksByAuthor = (books, searchAuthor) => {
+  console.log(`%c"${searchAuthor}"`, "background-color: #9e2a2b; color: white; font-size: 10px; text-align: center ; padding: 10px ;  margin: 5px ; font-style: italic")
+  const authorFilter = books.filter((books) => books.author === searchAuthor)
+  console.table(authorFilter)
+  console.log(authorFilter)
+}
+
+filterBooksByAuthor(books, searchAuthor)
 
 //Segunda parte: a partir de lo realizado en la primera parte, utilizar la función para obtener todos los libros de un autor a elección, luego mediante métodos de array modificar el nombre del autor de todos los libros filtrados. Observar que si se muestra en consola los libros filtrados se verá el autor modificado. ¿Qué sucede si se muestra en consola el array original?
 
