1ff2ae633dd37fb26fd3229b26794ef8cd79b434
MCeciliaLuna <lunama.cecilia@live.com>
2023-05-29 18:19:40 -0300

    di estilos para entender los resultados en consola

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-console.log("%cBooks & Authors", "background-color: #ee9b00; color: white; font-size: 20px; text-align: center ; padding: 10px ;  margin: 5px")
-
 const books = [
   { title: 'El Aleph', author: 'Jorge Luis Borges' },
   { title: 'La ciudad y los perros', author: 'Mario Vargas Llosa' },
@@ -19,17 +17,11 @@ const books = [
 
 console.table(books)
 
-//Primera parte: dado un array de libros, implementar una función filterBooksByAuthor( ) que reciba por parámetros dicho array y un string con el nombre de un autor, y devuelva un nuevo array con libros del autor indicado.
-
-console.log("%cSearch by Authors", "background-color: #ee9b00; color: white; font-size: 15px; text-align: center ; padding: 10px ;  margin: 5px")
-
 let searchAuthor = 'Gabriel García Márquez'
+console.log(`\nLibros del autor "${searchAuthor}":`)
 
 const filterBooksByAuthor = (books, searchAuthor) => {
-  console.log(`%c"${searchAuthor}"`, "background-color: #9e2a2b; color: white; font-size: 10px; text-align: center ; padding: 10px ;  margin: 5px ; font-style: italic")
   const authorFilter = books.filter((books) => books.author === searchAuthor)
-
-  //Segunda parte: a partir de lo realizado en la primera parte, utilizar la función para obtener todos los libros de un autor a elección, luego mediante métodos de array modificar el nombre del autor de todos los libros filtrados. Observar que si se muestra en consola los libros filtrados se verá el autor modificado. ¿Qué sucede si se muestra en consola el array original?
 
   const authorFilteredBooks = authorFilter.map((book) => book.title)
   const inventedAuthor = 'Cecilia'
@@ -37,19 +29,13 @@ const filterBooksByAuthor = (books, searchAuthor) => {
     return {title, author: inventedAuthor}
   })
 
-  console.table(authorFilter)
-  console.log(authorFilteredBooks)
+  console.table(authorFilteredBooks)
+  console.log(`\nAutor cambiado por "${inventedAuthor}":`)
   console.table(authorChanged)
 }
 
 filterBooksByAuthor(books, searchAuthor)
 
+console.log(`\nEl array original NO HA SIDO MODIFICADO`)
 console.table(books)
-
-
-//Tercera parte: modificar la función de la primera parte para que ahora funcione si mando el nombre completo del autor, o solo su apellido. Por ejemplo, la función debe devolver lo siguiente, ya sea si le mando ‘Jorge Luis Borges’ o ‘Borges’: 
-
-
-
-//Cuarta parte: teniendo en cuenta lo observado en la segunda parte, corregir ese comportamiento de la forma que considere apropiada.
 
